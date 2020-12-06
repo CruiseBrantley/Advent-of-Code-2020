26ca9aa135a93853b4ff74cb3b4a130ddec98d3c
Cruise Brantley <cruise.brantley@gmail.com>
2020-12-06 16:02:37 -0600

    Sixth Day Solved

---
+++ sixth/index.js
@@ -0,0 +1,21 @@
+const input = require('./data')
+const splitGroups = input.split(/\n\n/m)
+const splitValues = splitGroups.map(group => group.split(/\n/g))
+
+let totalValue = 0
+let totalAgreedValue = 0
+splitValues.map(group => {
+  const groupSetValues = new Set()
+  const groupObjectValues = {}
+  group.forEach((member, idx) => Array.from(member).forEach(item => {
+    groupSetValues.add(item)
+    if (!(item in groupObjectValues)) groupObjectValues[item] = []
+    groupObjectValues[item].push(idx)
+  }))
+  totalValue += groupSetValues.size
+  for (const value in groupObjectValues) {
+    if (groupObjectValues[value].length === group.length) totalAgreedValue += 1
+  }
+})
+console.log('Part 1:', totalValue)
+console.log('Part 2:', totalAgreedValue)
